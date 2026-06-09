7ecd2449cef85707088a3cc34a814b034e8da844
Anshika Bhanot <anshikabhanot@Anshikas-MacBook-Air.local>
2026-06-09 15:30:27 +0100

    Fix __future__ import position

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,10 +1,10 @@
+from __future__ import annotations
+
 import sys
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
 sys.path.append(str(ROOT_DIR / "src"))
-
-from __future__ import annotations
 
 import json
 
